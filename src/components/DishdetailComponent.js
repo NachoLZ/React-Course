69185b2bff516a7c878cb58afb568bf8d1ccf7ba
Ignacio Lizana <ignacio.lizanaa@sansano.usm.cl>
2020-09-09 01:50:59 -0300

    Assignment 3 solution

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,116 @@
-import React from 'react';
-import { Card, CardImg, CardText , CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText , CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Form, FormGroup, Input, Modal, ModalHeader, ModalBody, Label, Col, Button, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
+    class CommentForm extends Component {
+
+        constructor(props){
+            super(props);
+            this.state = {
+                isModalOpen : false,
+                author: '',
+                rating: '',
+                comment: '',
+                touched: {
+                    author: false,
+                    rating: false,
+                    comment: false
+                }
+            }
+            this.handleSubmit = this.handleSubmit.bind(this);
+            this.handleInputChange = this.handleInputChange.bind(this);
+            this.handleBlur = this.handleBlur.bind(this);
+            this.toggleModal = this.toggleModal.bind(this);
+        }
+    
+        handleInputChange(event) {
+            const target = event.target;
+            const value = target.type === 'checkbox' ? target.checked : target.value;
+            const name = target.name;
+    
+            this.setState({
+                [name]: value
+            });
+        }
+
+        toggleModal() {
+            this.setState({
+                isModalOpen :!this.state.isModalOpen
+            });
+        }
+    
+        handleSubmit(event){
+            this.toggleModal();
+            alert("Author: " + this.author.value + " Rating: " + this.rating.value + " Comment: " + this.comment.value);
+            event.preventDefault();
+        }
+
+        handleBlur = (field) => (evt) => {
+            this.setState({
+                touched: {...this.state.touched, [field]: true }
+            });
+        }
+
+        validate(author) {
+            const errors = {
+                author: ''
+            };
+    
+            if (this.state.touched.author && author.length < 2)
+                errors.author = 'Must be greater than 2 characters.';
+            else if (this.state.touched.author && author.length > 15)
+                errors.author = 'Must be 15 characters or less';
+            
+                return errors;
+        }
+
+        render(){
+            const errors = this.validate(this.state.author);
+            return(
+                <React.Fragment>
+                <Button outline onClick={this.toggleModal}>
+                        <span className="fa fa-pencil fa-lg">Submit Comment</span>
+                </Button>  
+                <Modal isOpen ={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader  toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup>
+                                <Label htmlFor="rating">Rating</Label>
+                                <Input type ="select" id="rating" name="rating" innerRef={(input) => this.rating = input}>
+                                    onChange={this.handleInputChange} 
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="author">Your Name</Label>
+                                <Input type ="text" id="author" name="author" innerRef={(input) => this.author = input} onBlur={this.handleBlur('author')}
+                                        onChange={this.handleInputChange} valid={errors.author === ''}
+                                        invalid={errors.author !== ''}/>
+                                <FormFeedback>{errors.author}</FormFeedback>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Input type="textarea" id="comment" name="comment"
+                                        rows="12"
+                                        innerRef={(input) => this.comment = input}
+                                        onChange={this.handleInputChange}  />
+                                </Col>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+                </React.Fragment>
+            );
+        }
+    }
 
     function RenderDish({dish}) {
 
@@ -17,7 +125,6 @@ import { Link } from 'react-router-dom';
             );
         
     }
-
 
     function RenderComments({comments}){
             const comm = comments.map((c) => { 
@@ -53,6 +160,7 @@ import { Link } from 'react-router-dom';
                         <CardBody>
                             <CardText>{comm}</CardText>
                         </CardBody>
+                        <CommentForm /> 
                 </Card>
             );   
  
