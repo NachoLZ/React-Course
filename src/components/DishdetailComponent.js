8bc1cac0d38b19eef747ac00aec48f219422f273
Ignacio Lizana <ignacio.lizanaa@sansano.usm.cl>
2020-09-21 20:40:46 -0300

    final sin feedback server 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import { Control } from 'react-redux-form';
-import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+import { FadeTransform } from 'react-animation-components';
 
 
 
